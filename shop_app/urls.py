00f90e9086bed681630bcec0c5a95439ae9c67b1
Evgenii Morozov <zheka1101@yandex.ru>
2021-09-09 09:18:29 +0500

    make migration

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import *
-
+from . import views
 
 app_name = 'shop_app'
 
 urlpatterns = [
-    path('/', admin.site.urls),
+    path('', views.index, name='index'),
+    # path('about', views.about),
+    # path('create', views.create),
+    # path('delete/<int:id>', views.delete),
+    # path('buy/<int:id>', views.buy),
 ]
