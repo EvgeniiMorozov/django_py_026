01d967ea28d1b1ff1eef58b8b9388e670aff8e54
EvgeniiMorozov <emorozov1101@gmail.com>
2021-09-22 20:47:38 +0500

    modify

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from .weather import *
+
 
 def index(request):
-    return render(request, 'weather_app/index.html')
+    return render(request, "weather_app/index.html")
--- weather_app/weather.py
+++ weather_app/weather.py
@@ -1,28 +1,17 @@
 import requests
 
 
-WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
-WEATHER_API_KEY = '2067eeaf5536086acde12a04f8037f47'
+WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+WEATHER_API_KEY = "2067eeaf5536086acde12a04f8037f47"
 
 
 def one_call_weather(latitude, longitude):
-    url = WEATHER_API_URL + '/onecall?'
-    payload = {
-        'lat': latitude,
-        'lon': longitude,
-        'units': 'metric',
-        'lang': 'ru',
-        'appid': WEATHER_API_KEY
-    }
+    url = WEATHER_API_URL + "/onecall?"
+    payload = {"lat": latitude, "lon": longitude, "units": "metric", "lang": "ru", "appid": WEATHER_API_KEY}
     return requests.get(url, payload).json()
 
 
 def city_weather_call(city_name):
-    url = WEATHER_API_URL + '/weather?'
-    payload = {
-        'q': city_name,
-        'units': 'metric',
-        'lang': 'ru',
-        'appid': WEATHER_API_KEY
-    }
+    url = WEATHER_API_URL + "/weather?"
+    payload = {"q": city_name, "units": "metric", "lang": "ru", "appid": WEATHER_API_KEY}
     return requests.get(url, payload).json()
