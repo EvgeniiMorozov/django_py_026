07c8e8edc1cb9e93984f485cddba43ded87efcc4
Evgenii Morozov <zheka1101@yandex.ru>
2021-09-23 13:07:29 +0500

    make migration

--- weather_app/models.py
+++ weather_app/models.py
@@ -6,7 +6,7 @@ class CityWeather(models.Model):
     latitude = models.DecimalField(max_digits=4, decimal_places=4, verbose_name="Широта")
     longitude = models.DecimalField(max_digits=4, decimal_places=4, verbose_name="Долгота")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Обновлено")
-    weather_condition = models.JSONField(null=True, verbose_name="Погода (JSON)")
+    weather_json = models.JSONField(null=True, verbose_name="Погода (JSON)")
 
     def __str__(self) -> str:
         return self.name
