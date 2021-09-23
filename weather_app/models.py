844e9849abe324997f07e96a6af2ffec31304f34
Evgenii Morozov <zheka1101@yandex.ru>
2021-09-23 12:27:04 +0500

    add migration

--- weather_app/models.py
+++ weather_app/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class CityWeather(models.Model):
     name = models.CharField(max_length=255, unique=True, verbose_name="Город")
-    latitude = models.DecimalField(decimal_places=4, verbose_name="Широта")
-    longitude = models.DecimalField(decimal_places=4, verbose_name="Долгота")
+    latitude = models.DecimalField(max_digits=4, decimal_places=4, verbose_name="Широта")
+    longitude = models.DecimalField(max_digits=4, decimal_places=4, verbose_name="Долгота")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Обновлено")
     weather_condition = models.JSONField(null=True, verbose_name="Погода (JSON)")
 
