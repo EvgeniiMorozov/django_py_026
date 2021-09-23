a649561b8ec76f45bb575f113b90b4f5cdac43c8
Evgenii Morozov <zheka1101@yandex.ru>
2021-09-23 12:23:05 +0500

    add model

--- weather_app/models.py
+++ weather_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class CityWeather(models.Model):
+    name = models.CharField(max_length=255, unique=True, verbose_name="Город")
+    latitude = models.DecimalField(decimal_places=4, verbose_name="Широта")
+    longitude = models.DecimalField(decimal_places=4, verbose_name="Долгота")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Обновлено")
+    weather_condition = models.JSONField(null=True, verbose_name="Погода (JSON)")
+
+    def __str__(self) -> str:
+        return self.name
