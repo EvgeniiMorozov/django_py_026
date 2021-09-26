43b36946004a713208e09ff8d373293e5ba14d41
EvgeniiMorozov <emorozov1101@gmail.com>
2021-09-26 12:01:11 +0500

    modify

--- django_py_026/urls.py
+++ django_py_026/urls.py
@@ -27,5 +27,4 @@ urlpatterns = [
     path("weather/", include("weather_app.urls")),
 ]
 if settings.DEBUG:
-    # urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
