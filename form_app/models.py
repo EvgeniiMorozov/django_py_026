f18f2f5a4afcbe7c7c02242dc5f8dae61bea83de
Evgenii Morozov <zheka1101@yandex.ru>
2021-10-01 12:22:48 +0500

    add validation

--- form_app/models.py
+++ form_app/models.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from django.db import models
+from django.urls import reverse
 
 
 class MaxResolutionErrorException(Exception):
@@ -18,20 +19,23 @@ class User(models.Model):
     last_name = models.CharField(max_length=45, verbose_name="Фамилия")
     email = models.EmailField(max_length=254, verbose_name="Электронная почта")
     photo = models.ImageField(upload_to="form_app/photos", verbose_name="Фотография")
-    slug = models.SlugField(max_length=80, verbose_name="SLUG", unique=True)
+    slug = models.SlugField(max_length=80, verbose_name="SLUG", unique=True, db_index=True)
 
     def __str__(self):
         return f"Пользователь {self.first_name} {self.last_name}"
 
-    # def save(self, *args, **kwargs):
-    #     photo = self.photo
-    #     img = Image.open(photo)
-    #     max_width, max_height = self.MAX_PHOTO_RESOLUTION
-    #
-    #     if img.width > max_width or img.height > max_height:
-    #         raise MaxResolutionErrorException("Разрешение фото больше максимального!")
-    #
-    #     if img.size > User.MAX_PHOTO_SIZE:
-    #         raise MaxSizeErrorException("Размер файла фотографии больше допустимого размера!")
-    #
-    #     super(User, self).save(*args, **kwargs)
+    def get_absolute_url(self):
+        return reverse("form_app:users", kwargs={"slug": self.slug})
+
+    def save(self, *args, **kwargs):
+        photo = self.photo
+        img = Image.open(photo)
+        max_width, max_height = self.MAX_PHOTO_RESOLUTION
+
+        if img.width > max_width or img.height > max_height:
+            raise MaxResolutionErrorException("Разрешение фото больше максимального!")
+
+        if img.size > User.MAX_PHOTO_SIZE:
+            raise MaxSizeErrorException("Размер файла фотографии больше допустимого размера!")
+
+        super(User, self).save(*args, **kwargs)
