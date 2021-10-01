bb195e2905f9aedc86baf17a69efa979f9671a27
Evgenii Morozov <zheka1101@yandex.ru>
2021-10-01 12:54:21 +0500

    add validation in model

--- form_app/models.py
+++ form_app/models.py
@@ -1,6 +1,8 @@
+from re import IGNORECASE
 from PIL import Image
 from django.db import models
 from django.urls import reverse
+from django.core.validators import RegexValidator, EmailValidator, FileExtensionValidator
 
 
 class MaxResolutionErrorException(Exception):
@@ -14,10 +16,19 @@ class MaxSizeErrorException(Exception):
 class User(models.Model):
     MAX_PHOTO_RESOLUTION = (800, 800)
     MAX_PHOTO_SIZE = 3145728
+    VALIDATORS = {
+        "regex": RegexValidator(
+            regex=r"^\w", message="Имя|Фамилия должно начинаться с только с буквы!", code="invalid", flags=IGNORECASE
+        ),
+        "email": EmailValidator(
+            message="Разрешённые почтовые домены: mail.ru, yandex.ru, gmail.com",
+            allowlist=["mail.ru", "yandex.ru", "gmail.com"],
+        ),
+    }
 
-    first_name = models.CharField(max_length=30, verbose_name="Имя")
-    last_name = models.CharField(max_length=45, verbose_name="Фамилия")
-    email = models.EmailField(max_length=254, verbose_name="Электронная почта")
+    first_name = models.CharField(max_length=30, verbose_name="Имя", validators=[VALIDATORS["regex"]])
+    last_name = models.CharField(max_length=45, verbose_name="Фамилия", validators=[VALIDATORS["regex"]])
+    email = models.EmailField(max_length=254, verbose_name="Электронная почта", validators=VALIDATORS["email"])
     photo = models.ImageField(upload_to="form_app/photos", verbose_name="Фотография")
     slug = models.SlugField(max_length=80, verbose_name="SLUG", unique=True, db_index=True)
 
