d41dd8ffa7bd851449933ed7dbad069588129cf1
EvgeniiMorozov <emorozov1101@gmail.com>
2021-10-24 12:33:06 +0500

    add route for django debug toolbar

--- django_py_026/urls.py
+++ django_py_026/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 # from .settings import DEBUG, MEDIA_ROOT, MEDIA_URL
 
 urlpatterns = [
@@ -29,4 +30,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns.append(path("__debug__/", include(debug_toolbar.urls)))
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
